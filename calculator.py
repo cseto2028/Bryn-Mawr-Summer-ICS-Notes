f3c2b3c3364a6001884f0f612f2b325a7bf8f3c5
cseto2028 <setoc@brynmawrschool.org>
2025-06-17 10:43:53 -0400

    add new file

---
+++ calculator.py
@@ -0,0 +1,19 @@
+num1= 1
+num2= 2
+print(num1 +num2)
+sum= 3
+print (sum)
+num5= 5
+num6= 6
+print(num6/num5)
+sum1= 1.2
+print(sum1) 
+num7= 7
+num8= 8
+print(num8-num7)
+sum2= 1
+print(sum2)
+num9= 9
+num10= 10
+print("modular division")
+print(num10%num9)
---
+++ test.py
@@ -0,0 +1,17 @@
+print("Hello World")
+print(5+10)
+number= 10 
+print(number)
+num2= 5 
+print("Sum of number and num2")
+print(number + num2)
+sum= number + num2 
+print("sum variable")
+print(sum)
+name= "Chloe Seto"
+print("printing name")
+print(name)
+number= 10 
+print(number)
+number= 25 
+print(number)
