0d8d76342898a1b4e6e34c9501fd5c7bbc3a9eaa
cseto2028 <setoc@brynmawrschool.org>
2025-06-17 23:37:13 -0400

    Homework due 6/18

--- calculator.py
+++ calculator.py
@@ -1,19 +1,20 @@
-num1= 1
-num2= 2
-print(num1 +num2)
-sum= 3
-print (sum)
-num5= 5
-num6= 6
-print(num6/num5)
-sum1= 1.2
-print(sum1) 
-num7= 7
-num8= 8
-print(num8-num7)
-sum2= 1
-print(sum2)
-num9= 9
-num10= 10
-print("modular division")
-print(num10%num9)
+num1= int(input("type in num1: "))
+num2= int(input("type in num2: "))
+sum= num1 +num2 
+print(f"the sum of {num1} and {num2} is {sum}")
+num3= int(input("type in num3: "))
+num4= int(input("type in num4: "))
+product= num3 * num4 
+print(f"the product of {num3} and {num4} is {product} ")
+num5= int(input("type in num5: "))
+num6= int(input("type in num6: "))
+sum1= num6 - num5
+print(f"the sum of {num6 } and {num5} is {sum1} ")
+num7= int(input("type in num7: "))
+num8= int(input("type in num8: "))
+quotient= num8/num7 
+print(f"the quotient of {num7} and {num8} is {quotient}")
+num9= int(input("type in num9: "))
+num10= int(input("type in num10: "))
+remainder= num10%num9 
+print(f"the remainder of {num10} and {num9} is {remainder}")
